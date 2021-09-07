ccfc6d5d09af221fbc25551f87cb1869fbee62cd
Shourya Gupta <gptshourya02@gmail.com>
2021-09-07 17:16:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+const nameInput = document.getElementById('name')
+const downloadBtn = document.getElementById('download-btn')
+
+const image = new Image()
+image.src = './certificate.jpg'
+image.onload = function () {
+	drawImage()
+}
+
+function drawImage() {
+	// ctx.clearRect(0, 0, canvas.width, canvas.height)
+	ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+	ctx.font = '90px monotype corsiva'
+	ctx.fillStyle = '#29e'
+	ctx.textAlign = 'center';
+	ctx.textBaseline = 'middle';
+	ctx.fillText(nameInput.value, 480, 460)
+}
+
+nameInput.addEventListener('input', function () {
+	drawImage()
+})
+
+downloadBtn.addEventListener('click', function () {
+	downloadBtn.href = canvas.toDataURL('image/jpg')
+	downloadBtn.download = 'Certificate - ' + nameInput.value
+})
